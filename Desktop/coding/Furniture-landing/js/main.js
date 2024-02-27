703853180e9146a0126bb38174e417c85591a02d
allypetroova <allypetroovaa@gmail.com>
2024-02-27 17:29:04 +0300

    5 commit

---
+++ Desktop/coding/Furniture-landing/js/main.js
@@ -0,0 +1,13 @@
+    const swiper = new Swiper('.swiper', {
+
+        loop: true,
+        freeMode: true,
+
+        slidesPerView: 4,
+        spaceBetween: 42,
+
+        navigation: {
+          nextEl: '#sliderNext',
+          prevEl: '#sliderPrev',
+        },
+      });
