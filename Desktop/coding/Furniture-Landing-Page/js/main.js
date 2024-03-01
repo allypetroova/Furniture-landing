33e052c32c5522fad3d57d22e3addf8710f1492a
allypetroova <allypetroovaa@gmail.com>
2024-03-01 17:11:11 +0300

    8 commit

--- Desktop/coding/Furniture-Landing-Page/js/main.js
+++ Desktop/coding/Furniture-Landing-Page/js/main.js
@@ -11,3 +11,18 @@ const swiper = new Swiper('.swiper', {
       prevEl: '#sliderPrev',
     },
   });
+
+
+// Mobile Nav
+
+const mobileNavOpenBtn = document.querySelector('#open-mobile-nav-btn');
+const mobileNavCloseBtn = document.querySelector('#close-mobile-nav-btn');
+const mobileNav = document.querySelector('#mobile-nav');
+
+mobileNavOpenBtn.onclick = function (){
+  mobileNav.classList.add('mobile-nav-wrapper--open');
+}
+
+mobileNavCloseBtn.onclick = function () {
+  mobileNav.classList.remove('mobile-nav-wrapper--open');
+};
